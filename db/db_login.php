733a98d498e10fb2b3f5c0632c79dcd576250be8
unknown <foysal.int@gmail.com>
2019-01-10 21:10:27 -0800

    First

---
+++ db/db_login.php
@@ -0,0 +1,29 @@
+<?php
+include('db.php');
+
+// $email=$_POST['email'];
+// $password=$_POST['password'];
+session_start();
+
+if (isset($_POST['login'])) {
+	$email = mysqli_real_escape_string($con, $_POST['email']);
+	$password=mysqli_real_escape_string($con, $_POST['password']);
+	$result=mysqli_query($con,"SELECT * FROM users where email='$email' and password='$password'");
+	// $result1=mysqli_query($con,"SELECT * FROM employee where username='$username' and password='$password'");
+	if ($row = mysqli_fetch_array($result)) {
+		$_SESSION['uid']=$row['uid'];
+		$_SESSION['name']=$row['name'];		
+		$_SESSION['email']=$row['email'];
+		$_SESSION['level']=$row['level'];
+		$_SESSION['sm_staff']=TRUE;	
+		header("Location:../index");
+				
+	}else{
+		header("Location:../login/login?wrong=1");
+	}
+	
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,192 @@
+<?php
+session_start();
+if ($_SESSION['sm_staff']==TRUE) {
+  
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+
+<?php include('inc/head.php');?>
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+  <?php include('inc/left_bar.php');?>
+    <!-- Start Welcome area -->
+    <div class="all-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="logo-pro">
+                        <a href="index.html"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+   <?php include('inc/top_bar.php');?>
+        <div class="section-admin container-fluid">
+		
+            <div class="row admin text-center">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                            <div class="admin-content analysis-progrebar-ctn res-mg-t-15">
+                                <h4 class="text-left text-uppercase"><b>Orders</b></h4>
+                                <div class="row vertical-center-box vertical-center-box-tablet">
+                                    <div class="col-xs-3 mar-bot-15 text-left">
+                                        <label class="label bg-green">30% <i class="fa fa-level-up" aria-hidden="true"></i></label>
+                                    </div>
+                                    <div class="col-xs-9 cus-gh-hd-pro">
+                                        <h2 class="text-right no-margin">10,000</h2>
+                                    </div>
+                                </div>
+                                <div class="progress progress-mini">
+                                    <div style="width: 78%;" class="progress-bar bg-green"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12" style="margin-bottom:1px;">
+                            <div class="admin-content analysis-progrebar-ctn res-mg-t-30">
+                                <h4 class="text-left text-uppercase"><b>Tax Deduction</b></h4>
+                                <div class="row vertical-center-box vertical-center-box-tablet">
+                                    <div class="text-left col-xs-3 mar-bot-15">
+                                        <label class="label bg-red">15% <i class="fa fa-level-down" aria-hidden="true"></i></label>
+                                    </div>
+                                    <div class="col-xs-9 cus-gh-hd-pro">
+                                        <h2 class="text-right no-margin">5,000</h2>
+                                    </div>
+                                </div>
+                                <div class="progress progress-mini">
+                                    <div style="width: 38%;" class="progress-bar progress-bar-danger bg-red"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                            <div class="admin-content analysis-progrebar-ctn res-mg-t-30">
+                                <h4 class="text-left text-uppercase"><b>Revenue</b></h4>
+                                <div class="row vertical-center-box vertical-center-box-tablet">
+                                    <div class="text-left col-xs-3 mar-bot-15">
+                                        <label class="label bg-blue">50% <i class="fa fa-level-up" aria-hidden="true"></i></label>
+                                    </div>
+                                    <div class="col-xs-9 cus-gh-hd-pro">
+                                        <h2 class="text-right no-margin">$70,000</h2>
+                                    </div>
+                                </div>
+                                <div class="progress progress-mini">
+                                    <div style="width: 60%;" class="progress-bar bg-blue"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                            <div class="admin-content analysis-progrebar-ctn res-mg-t-30">
+                                <h4 class="text-left text-uppercase"><b>Yearly Sales</b></h4>
+                                <div class="row vertical-center-box vertical-center-box-tablet">
+                                    <div class="text-left col-xs-3 mar-bot-15">
+                                        <label class="label bg-purple">80% <i class="fa fa-level-up" aria-hidden="true"></i></label>
+                                    </div>
+                                    <div class="col-xs-9 cus-gh-hd-pro">
+                                        <h2 class="text-right no-margin">$100,000</h2>
+                                    </div>
+                                </div>
+                                <div class="progress progress-mini">
+                                    <div style="width: 60%;" class="progress-bar bg-purple"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
+
+
+
+  
+       <!--  <div class="calender-area mg-tb-30">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="calender-inner">
+                            <div id='calendar'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <div class="footer-copyright-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="footer-copy-right">
+                            <p>Copyright &copy; <?php echo date('Y'); ?> Sonargoan Motors All rights reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jquery
+		============================================ -->
+    <script src="js/vendor/jquery-1.11.3.min.js"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="js/wow.min.js"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="js/jquery-price-slider.js"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="js/jquery.meanmenu.js"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- sticky JS
+		============================================ -->
+    <script src="js/jquery.sticky.js"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/scrollbar/mCustomScrollbar-active.js"></script>
+    <!-- metisMenu JS
+		============================================ -->
+    <script src="js/metisMenu/metisMenu.min.js"></script>
+    <script src="js/metisMenu/metisMenu-active.js"></script>
+    <!-- morrisjs JS
+		============================================ -->
+    <script src="js/morrisjs/raphael-min.js"></script>
+    <script src="js/morrisjs/morris.js"></script>
+    <script src="js/morrisjs/morris-active.js"></script>
+    <!-- morrisjs JS
+		============================================ -->
+    <script src="js/sparkline/jquery.sparkline.min.js"></script>
+    <script src="js/sparkline/jquery.charts-sparkline.js"></script>
+    <!-- calendar JS
+		============================================ -->
+    <script src="js/calendar/moment.min.js"></script>
+    <script src="js/calendar/fullcalendar.min.js"></script>
+    <script src="js/calendar/fullcalendar-active.js"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="js/plugins.js"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
+<?PHP
+
+}else{
+    header('location:login/login');
+}
+
+?>
